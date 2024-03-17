a3d3850b29add08cb53b6c0680c66653436b61fd
lanuchal <lanuchal.lkaol@gmail.com>
2024-03-17 16:53:37 +0700

    asd

--- menu.py
+++ menu.py
@@ -14,23 +14,23 @@ def create_menu(root, switch_to_page):
         # create button
     button_options = {
         "borderwidth": "0",
-        "font": ("Times", 30),
+        "font": ("Kanit", 40),
         "fg": "#ffffff",
         "justify": "center",
         "relief": "raised",
     }
 
     btn_sell = tk.Button(menu_page, **button_options,bg="#1e90ff", text="หน้าจอขาย", cursor="hand2", command=lambda: switch_to_page("user"))
-    btn_sell.place(x=160,y=150,width=300,height=120)
+    btn_sell.place(x=130,y=110,width=350,height=150)
 
     btn_stock = tk.Button(menu_page, **button_options,bg="#1e90ff", text="สินค้า/สต็อค", cursor="hand2", command=lambda: switch_to_page("stock"))
-    btn_stock.place(x=580,y=150,width=300,height=120)
+    btn_stock.place(x=580,y=110,width=350,height=150)
 
     btn_report = tk.Button(menu_page, **button_options,bg="#1e90ff", text="รายงาน", cursor="hand2", command=lambda: switch_to_page("report"))
-    btn_report.place(x=160,y=350,width=300,height=120)
+    btn_report.place(x=130,y=340,width=350,height=150)
 
     btn_report = tk.Button(menu_page, **button_options,bg="#1e90ff", text="ตั้งค่า", cursor="hand2", command=lambda: switch_to_page("setting"))
-    btn_report.place(x=580,y=350,width=300,height=120)
+    btn_report.place(x=580,y=340,width=350,height=150)
 
 
     return menu_page
